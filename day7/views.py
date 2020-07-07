dceaf7716b5b9079fc79a8404597c69a0e91d04b
e <2812511629@qq.com>
2020-07-07 19:45:36 +0800

    搜索/分页

--- day7/views.py
+++ day7/views.py
@@ -3,13 +3,17 @@ import re
 from django.shortcuts import render
 
 # Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter,OrderingFilter
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
 
 from day7.authentication import JWTAuthentication
-from day7.models import User
-from day7.serializers import UserModelSerializer
+from day7.filter import ComputerFilterSet, LimitFilter
+from day7.models import User, Computer
+from day7.serializers import UserModelSerializer, ComputerModelSerializer
 from utils.response import APIResponse
 
 
@@ -54,3 +58,25 @@ class LoginAPIView(APIView):
             return APIResponse(results={"username": user_obj.username}, token=token)
 
         return APIResponse(data_message="出错了")
+
+
+# 游标分页
+class ComputerListAPIView(ListAPIView):
+    queryset = Computer.objects.all()
+    serializer_class = ComputerModelSerializer
+
+    # 通过此参数配置过滤的器类
+    filter_backends = [SearchFilter, OrderingFilter, LimitFilter, DjangoFilterBackend]
+    # 指定当前搜索条件
+    search_fields = ["name", "price"]
+    # 指定排序的条件
+    ordering = ["price"]
+
+    # django-filter 查询   通过filter_class指定过滤器
+    filter_class = ComputerFilterSet
+
+    # 指定分页器   不能使用列表 或 元祖指定
+    # pagination_class = MyPageNumberPagination
+    # pagination_class = MyLimitPagination
+    # 127.0.0.1:8000/api/computers/?ordering=price&page_size=1&name=小米
+    # pagination_class = MyCoursePagination
